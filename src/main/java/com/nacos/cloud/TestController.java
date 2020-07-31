f1ea1ebccc4ea290c8e3a499cdd0b4dc72571954
jizc <540653764@qq.com>
2020-07-31 17:07:19 +0800

    123

---
+++ src/main/java/com/nacos/cloud/TestController.java
@@ -0,0 +1,23 @@
+package com.nacos.cloud;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class TestController {
+
+    @Value("${test.name}")
+    private String name;
+
+    @Value("${test.age}")
+    private int age;
+
+    @RequestMapping("/test")
+    public String test(){
+        return "name" + name + "/" + "age" + age;
+    }
+}
